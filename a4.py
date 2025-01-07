8be6c39bfac9dc96cb28c7cec949449ad1d563e4
Eranda724 <erandajayasinghe724@gmail.com>
2025-01-08 03:05:51 +0530

    update

--- a4.py
+++ a4.py
@@ -1,12 +1,14 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            j=i+1
+            for j in range(len(nums)):
+                if(nums[i]+nums[j]==target):
+                    return [i,j]
         return []
-
-solution = Solution()
-nums = list(map(int, input("Enter the array: ").strip("[]").split(',')))
-target = int(input("Enter the target: "))
-print(solution.twoSum(nums, target))
+    
+    solution = Solution()
+    nums = list(map(int, input().split()))
+    target = int(input())
+    print(Solution.twoSum(nums, target))

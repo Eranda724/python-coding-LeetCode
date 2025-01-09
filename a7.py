29a65cdf3c474f856d22bf51a24f69b31e550265
Eranda724 <erandajayasinghe724@gmail.com>
2025-01-09 21:46:04 +0530

    a1

--- a7.py
+++ a7.py
@@ -1,5 +1,34 @@
-import numpy as n
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        m == grid.length
-        n == grid[0].length
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    p = s.split(":")
+    if s[-2:] == "PM":
+        if p[0] != "12":
+            p[0] = str(int(p[0]) + 12)
+    
+    print(p[0])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
